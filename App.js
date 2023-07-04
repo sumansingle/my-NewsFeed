11a8dec39c62d2530e84e0d2b15e05065a2e1121
suman <ghoshs91439@gmail.com>
2023-07-04 14:24:06 +0530

    update newsfeed whole app

--- App.js
+++ App.js
@@ -6,92 +6,104 @@ const newNews = document.getElementById("newNews");
 let newsText =document.getElementById('news');
 const savedNewss=document.getElementById('savedNewsText');
 const selectFilter = document.getElementById("newsCategory");
-
+const Key = "k9r15XB6nHXWebziCvGglFlcp632wpLtkB3VF1L4RzXP6A7K";
 //this is fetch section
-fetch('https://inshorts.deta.dev/news?category=all')
+fetch(`https://api.currentsapi.services/v1/latest-news?apiKey=${Key}`)
 .then((response) => response.json())
 .then((json) => {
   renderNews(json);
+})
+.catch((error)=>{
+  console.log(error,"this is errorr")
 });
 
 selectFilter.addEventListener("change", function() {
-  let selectedCategory = selectFilter.value;
-  fetch(`https://inshorts.deta.dev/news?category=${selectedCategory}`)
-    .then((response) => response.json())
-    .then((json) => {
-      renderCategoryNews(json);
-    });
-  });
+ let selectedCategory = selectFilter.value;
+ fetch(`https://api.currentsapi.services/v1/latest-news?category=${selectedCategory}&apiKey=${Key}`)
+   .then((response) => response.json())
+  .then((json) => {
+    console.log(json)
+     renderCategoryNews(json);
+   });
+ });
 
   //this below section is function which show fetching data
 
 function renderNews(json) {
     let allContent = "";
-    let data = json.data;
-    let category = json.category;
+    let data = json.news;
+    //console.log(data);
+
     for (var i = 0; i < data.length; i++){
       const it = data[i]
-      allContent +=  
-      `<div class="main" style="background-color:#0f7ecd;color:black;padding:20px;margin:10px;font-family:popins;font-size: 1.2em;">
-      <div class="change">
-      <style>
-      .change{
-        background-color:white;
-      }
-        .change:hover{
-          background-color:black;
-          color: white;
-        } 
-      }
-      </style>
-      <h4 style="color:orange;font-family:serif">category: ${category}</h4>
-      <h2 style="color:orange">By ${it.author}</h2> 
-      <img src="${it.imageUrl}"height="300" width="500"></img>
-      <p style="color:orange">Date: ${it.date}</p>
-      <p>${it.content}<a href="${it.url}">read more</a></p> 
-      <button class="like-button" id="${i}">
-      </button>
-      </div>
-      </div>`;
+   allContent +=  
+    `<div class="change">
+   <style>
+   .change{
+      background-color:#303134;
+      color: white;
+      font-size : 16px;
+      border: 3px solid #3c4043;
+      border-radius: 20px;
+      box-shadow: 0 4px 10px rgba(1, 2, 4, 10);
+      transition: transform 0.8s ease;
+      
     }
-    newsText.innerHTML = allContent;
-    const buttons = newsText.querySelectorAll('.main .change .like-button');
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', function() {
-        loveButton(data[buttons[i].id]);
-      });
+      .change:hover{
+        transform: scale(1.1);
+      } 
     }
+    </style>
+    <h4 style="color:orange;font-family:serif">category: ${it.category}</h4>
+    <h4 style="color:orange">By ${it.author}</h4> 
+    <img src="${it.image}"height="200px" width="100%"></img>
+    <p style="color:orange;">Date: ${it.published}</p>
+    <p style="color:#dddddd;line-height: 1.5">${it.description}<a href="${it.url}">read more</a></p> 
+    <button class="like-button" id="${i}">
+    </button>
+    </div>`;
+  }
+   newsText.innerHTML = allContent;
+   const buttons = newsText.querySelectorAll('.main .change .like-button');
+   for (let i = 0; i < buttons.length; i++) {
+     buttons[i].addEventListener('click', function() {
+       loveButton(data[buttons[i].id]);
+     });
+   }
   }
   
   //this is category section
 
   function renderCategoryNews(json) {
-    let allContent = "";
-    let data = json.data;
-    let category = json.category;
+   let allContent = "";
+    let data = json.news;
     for (var i = 0; i < data.length; i++){
-      const it = data[i]
-      allContent +=  
-      `<div class="main" style="background-color:#0f7ecd;color:black;padding:20px;margin:10px;font-family:popins;font-size: 1.2em;">
-      <div class="change">
-      <style>
-      .change{
-        background-color:white;
+     const it = data[i]
+     allContent +=  
+     `<div class="change">
+     <style>
+     .change{
+        background-color:#303134;
+        color: white;
+        font-size : 15px;
+        border: 3px solid #3c4043;
+        border-radius: 20px;
+        box-shadow: 0 4px 10px rgba(1, 2, 4, 10);
+        transition: transform 0.8s ease;
+      
       }
         .change:hover{
-          background-color:black;
-          color: white;
+          transform: scale(1.1);
         } 
       }
       </style>
-      <h4 style="color:orange;font-family:serif">category: ${category}</h4>
-      <h2 style="color:orange">By ${it.author}</h2>
-       <img src="${it.imageUrl}"height="300" width="500"></img>
-       <p style="color:orange">Date: ${it.date}</p>
-      <p>${it.content}<a href="${it.url}">read more</a></p> 
+      <h4 style="color:orange;font-family:serif">category: ${it.category}</h4>
+      <h4 style="color:orange">By ${it.author}</h4> 
+      <img src="${it.image}"height="200px" width="100%"></img>
+      <p style="color:orange">Date: ${it.published}</p>
+      <p style="color:#dddddd;line-height: 1.5">${it.description}<a href="${it.url}">read more</a></p> 
       <button class="like-button" id="${i}">
       </button>
-      </div>
       </div>`;
     }
     newsText.innerHTML = allContent;
@@ -102,7 +114,6 @@ function renderNews(json) {
       });
     }
   }
-
 function myFunction() {
   newNews.style.display="none";
   savedNews.style.display="none";
@@ -118,19 +129,21 @@ loadSavedNewsBtn.addEventListener("click", function() {
 });
 
 loadNewNewsBtn.addEventListener("click", function() {
-  // Code to load new news
-  dashboard.style.display = "none";
+//  // Code to load new news
+ dashboard.style.display = "none";
     newNews.style.display = "block";
-    document.getElementById('loadNew1').style.backgroundColor = "grey";
+   document.getElementById('loadNew1').style.backgroundColor = "grey";
 });
 
 //this is button section
 
 function loveButton(data) {
   let author = data.author;
-  let content = data.content;
+  let description = data.description;
+  let url = data.url;
+  let image = data.image;
 const savedNews = JSON.parse(localStorage.getItem('loveButton')) || [];
- savedNews.push({ author,content });
+ savedNews.push({ author,description,image,url});
 localStorage.setItem('loveButton', JSON.stringify(savedNews));
 
 let likeButtons = document.getElementsByClassName('like-button');
@@ -141,32 +154,35 @@ let likeButtons = document.getElementsByClassName('like-button');
   }
 }
 
-//this is saved function
-
+////this is saved function
+//
 function savedFunction(){
   const savedNews = JSON.parse(localStorage.getItem('loveButton')) || [];
     let output = '';
-    savedNews.forEach(function(element) {
-      output += 
-      `<div style="background-color:#0f7ecd;color:black;padding:20px;margin:10px;font-family:popins;font-size: 1.2em;">
-      <div class="change">
+   savedNews.forEach(function(element) {
+     output += 
+      `<div class="change">
       <style>
       .change{
-        background-color:white;
+        background-color:#303134;
+        color: white;
+        font-size : 15px;
+        border: 3px solid #3c4043;
+        border-radius: 20px;
+        box-shadow: 0 4px 10px rgba(1, 2, 4, 10);
+        transition: transform 0.8s ease;
       }
         .change:hover{
-          background-color:black;
-          color: white;
+          transform: scale(1.1);
         } 
       }
       </style>
-      <h2 style="color:orange">By ${element.author}</h2>  
-      <p>${element.content}<a href="${element.url}">read more</a></p> 
-      <button class="like-button">
-      </button>
-      </div>
+      <h4 style="color:orange">By ${element.author}</h4>  
+      <img src="${element.image}"height="200px" width="100%"></img>
+      <p style="color:#dddddd;line-height: 1.5">${element.description}<a href="${element.url}">read more</a></p> 
+      <button class="like-button" style="background-color:darkred">
+     </button>
       </div>`;
-    });
-    
+    });    
     document.getElementById('savedNewsText').innerHTML = output;
 }
